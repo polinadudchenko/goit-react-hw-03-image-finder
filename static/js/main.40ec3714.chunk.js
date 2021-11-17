0a33993f9836c1c8fa31d8c20ff1aa3ba2b138df
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-17 19:49:28 +0100

    Updates

--- static/js/main.40ec3714.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),a=n.n(r),o=(n(8),n(9),n(0));var s=function(){return Object(o.jsx)("div",{className:"App"})};a.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.40ec3714.chunk.js.map
